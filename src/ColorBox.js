78f5b98c43ebf50037a2dae79928e4ece2c71ae0
Salvador Oliveira <slsalvadorlucas@gmail.com>
2022-02-01 23:49:51 -0400

    render colorbox with a random color, change the color onclick

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -3,21 +3,63 @@ import "./ColorBox.css";
 
 class ColorBox extends Component {
   static defaultProps = {
-    color: "black",
+    colorList: [
+      "black",
+      "silver",
+      "gray",
+      "white",
+      "maroon",
+      "red",
+      "purple",
+      "fuchsia",
+      "green",
+      "lime",
+      "olive",
+      "yellow",
+      "navy",
+      "blue",
+      "teal",
+      "aqua",
+      "orange",
+      "aliceblue",
+      "antiquewhite",
+      "aquamarine",
+      "azure",
+      "beige",
+      "bisque",
+      "blanchedalmond",
+      "blueviolet",
+      "brown",
+      "burlywood",
+      "cadetblue",
+      "chartreuse",
+    ],
   };
 
   constructor(props) {
     super(props);
     this.state = { color: "black" };
+    this.pickColor = this.pickColor.bind(this);
   }
 
   render() {
     return (
       <div
         className="ColorBox"
-        style={{ backgroundColor: this.props.color }}
+        style={{ backgroundColor: this.state.color }}
+        onClick={this.pickColor}
       ></div>
     );
+  }
+
+  pickColor() {
+    const { colorList } = this.props;
+    const rand = Math.floor(Math.random() * colorList.length);
+    this.setState(() => ({ color: colorList[rand] }));
+  }
+
+  componentDidMount() {
+    this.pickColor();
   }
 }
 
